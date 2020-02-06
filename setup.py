5b0f1cade9d123b16ad3a0be0441123385146f69
Sam Anklesaria <sam@spacemaker.ai>
2020-02-06 12:03:51 -0500

    Port package to git and python3

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from dynscope import __version__
+
+setup(
+    version = __version__,
+    description = "Dynamic Scope for Python",
+    author = "Georg Bauer",
+    author_email = "gb@rfc1437.de",
+    url = "http://bitbucket.org/rfc1437/dynscope/",
+    name='dynscope', 
+    long_description=open("README", 'r').read(),
+    license='MIT/X',
+    platforms=['BSD','Linux','MacOS X', 'win32'],
+    packages=['dynscope'],
+    scripts=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ]
+)
